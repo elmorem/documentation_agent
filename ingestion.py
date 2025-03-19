cce3df6cf86f81fc465adade67e9021f248c7138
elmorem <elmorem@gmail.com>
2025-03-19 14:48:40 -0700

    intial commit

---
+++ ingestion.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+from consts import INDEX_NAME
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+def ingest_docs():
+    pass
